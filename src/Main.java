5f56c23cd9811ff4f690c100916a839fe6b59676
jakekenefick <jake.kenefick24@d155.org>
2023-01-24 09:43:08 -0600

    Made a comment in IntelJ

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main
     public static void main(String[]args)
     {
       System.out.println("Made a comment in replit");
+      System.out.println("Made a comment in intelJ");
         JFrame myFrame = new JFrame();
         myFrame.setSize(500,500);
         //DrawPanel myPanel = new DrawPanel();
